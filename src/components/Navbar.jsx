3ff754717dda1deef58b9bf5d2b095fb711b30e6
Rabia-Elbeskeri <rabia.elebeskeri@gmail.com>
2025-04-02 14:19:11 +0100

    Task 14: Add live Netlify site link to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,24 @@
-import "./Navbar.css";
 import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <h1 className="navbar-title">NC News API Docs</h1>
-            <div className="navbar-links">
+            <h1>NC News API Docs</h1>
+            <div className="links">
                 <Link to="/">Home</Link>
-                <Link to="/api">API Reference</Link>
+                <Link to="/reference">API Reference</Link>
+                <a
+                    href="https://fabulous-choux-6677f0.netlify.app"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="external-link"
+                >
+                    Live Site
+                </a>
             </div>
         </nav>
     );
 };
 
 export default Navbar;
-
